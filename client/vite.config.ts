c180ead4f43b26f8e21c7fe0ea40428d59f87449
Mert <mert.izgahi@arneca.com>
2025-02-22 22:20:46 +0300

    fix-url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: 'https://feedbacks-app-mern.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
